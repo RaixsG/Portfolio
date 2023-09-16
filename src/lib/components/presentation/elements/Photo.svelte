2606c28be2f2d976f00bf946fd8c536bc34154f2
RaixsG <jhardelloficial@gmail.com>
2023-09-16 00:11:09 -0500

    feat: change to photo and styled

--- src/lib/components/presentation/elements/Photo.svelte
+++ src/lib/components/presentation/elements/Photo.svelte
@@ -1,18 +1,17 @@
-<script>
-</script>
+
 <div />
 
 <style>
     div {
         background-image: url("../../../../img/photosinFondo.png");
         max-width: 100%;
-        width: 250px;
+        width: 300px;
         right: 0;
         bottom: 0;
         background-repeat: no-repeat;
         background-size: cover;
-        border-bottom-left-radius: 20px;
-        transform: scaleX(-1);
+        /* border-bottom-left-radius: 20px; */
+        /* transform: scaleX(-1); */
     }
 </style>
 
